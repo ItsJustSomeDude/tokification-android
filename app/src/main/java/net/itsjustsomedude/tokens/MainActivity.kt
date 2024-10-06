81a51f36eee343ec2875cf4f40ce8066dc4e2ad5
Matthias Feldpausch <itsjustsomedude0@gmail.com>
2024-10-06 16:06:00 -0400

    Settings to Settings Activity

--- app/src/main/java/net/itsjustsomedude/tokens/MainActivity.kt
+++ app/src/main/java/net/itsjustsomedude/tokens/MainActivity.kt
@@ -5,15 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Add
@@ -22,15 +14,10 @@ import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -45,9 +32,8 @@ import androidx.compose.ui.unit.dp
 import net.itsjustsomedude.tokens.models.MainScreenViewModel
 import net.itsjustsomedude.tokens.ui.CoopList
 import net.itsjustsomedude.tokens.ui.EditCoop
+import net.itsjustsomedude.tokens.ui.Header
 import net.itsjustsomedude.tokens.ui.NotificationDebuggerDialog
-import net.itsjustsomedude.tokens.ui.SettingsScreen
-import net.itsjustsomedude.tokens.ui.theme.TokificationTheme
 import org.koin.compose.viewmodel.koinViewModel
 
 class MainActivity : ComponentActivity() {
@@ -73,6 +59,7 @@ private fun Content(model: MainScreenViewModel = koinViewModel()) {
     var showNotificationDebugger by remember { mutableStateOf(false) }
 
     Header(
+        title = { Text(stringResource(R.string.app_name)) },
         actions = {
             IconButton(onClick = {
                 model.refreshNotifications()
@@ -114,11 +101,6 @@ private fun Content(model: MainScreenViewModel = koinViewModel()) {
     ) {
         EditCoop(coopId = coop)
 
-        // TODO: Move _all_ this to a preferences screen.
-        HorizontalDivider()
-        Text("Eventually, this will be moved to a Settings screen, but right now that's not added so I tacked them on here.")
-        SettingsScreen()
-
         if (showCoopListSheet)
             ModalBottomSheet(onDismissRequest = { showCoopListSheet = false }) {
                 CoopList(
@@ -147,48 +129,5 @@ private fun Content(model: MainScreenViewModel = koinViewModel()) {
             NotificationDebuggerDialog(onDismissRequest = {
                 showNotificationDebugger = false
             })
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun Header(
-    navigation: @Composable () -> Unit = {},
-    actions: @Composable (RowScope.() -> Unit) = {},
-    content: @Composable ColumnScope.() -> Unit
-) {
-    TokificationTheme {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = { Text(stringResource(R.string.app_name)) },
-                    navigationIcon = navigation,
-
-                    actions = actions,
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.primary,
-                    ),
-                )
-            }
-        ) { paddingValues ->
-
-            Column(
-                modifier = Modifier
-                    .verticalScroll(rememberScrollState())
-                    .padding(
-                        PaddingValues(
-                            start = 8.dp,
-                            end = 8.dp,
-                            top = paddingValues.calculateTopPadding() + 8.dp,
-                            bottom = paddingValues.calculateBottomPadding(),
-                        )
-                    )
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                content()
-            }
-        }
     }
 }
--- app/src/main/java/net/itsjustsomedude/tokens/SettingsActivity.kt
+++ app/src/main/java/net/itsjustsomedude/tokens/SettingsActivity.kt
@@ -1,62 +1,39 @@
 package net.itsjustsomedude.tokens
 
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
-import net.itsjustsomedude.tokens.databinding.ActivitySettingsBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import net.itsjustsomedude.tokens.ui.Header
+import net.itsjustsomedude.tokens.ui.SettingsScreen
 
 private const val TAG = "SettingsActivity"
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val binding = ActivitySettingsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        title = "Options"
-
-        if (savedInstanceState == null)
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home)
-            finish()
-
-        return super.onOptionsItemSelected(item)
-    }
-}
-
-class SettingsFragment : PreferenceFragmentCompat() {
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey)
-
-//        findPreference<SwitchPreferenceCompat>("service_control_enable_service")?.apply {
-//            onPreferenceChangeListener =
-//                Preference.OnPreferenceChangeListener { _, newValue ->
-//                    val newState = newValue as Boolean
-//                    NotificationReader.setServiceEnabled(requireContext(), newState)
-//                    Log.i(TAG, "Setting service to $newState")
-//                    true
-//                }
-//        }
-//
-//        findPreference<Preference>("btn_show_template_info")?.apply {
-//            onPreferenceChangeListener =
-//                Preference.OnPreferenceChangeListener { _, _ ->
-//                    AlertDialog.Builder(requireContext())
-//                        .setTitle(R.string.settings_reports_header)
-//                        .setMessage(R.string.settings_templating_info)
-//                        .create().show()
-//                    false
-//                }
-//        }
+        enableEdgeToEdge()
+        setContent {
+            Header(
+                title = { Text("Settings") },
+                navigation = {
+                    IconButton(
+                        onClick = { finish() }
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                }
+            ) {
+                SettingsScreen()
+            }
+        }
     }
 }
