5d0e147d1197f720d148c9438fa526e213fa271c
Matthias Feldpausch <itsjustsomedude0@gmail.com>
2024-11-06 20:36:27 -0500

    Organize Report generation code.

--- app/src/main/java/net/itsjustsomedude/tokens/db/ExpandedCoop.kt
+++ app/src/main/java/net/itsjustsomedude/tokens/db/ExpandedCoop.kt
@@ -4,8 +4,8 @@ import net.itsjustsomedude.tokens.db.Coop.Companion.BOOST_ORDER_UNKNOWN
 import net.itsjustsomedude.tokens.roundedTval
 import net.itsjustsomedude.tokens.tval
 import java.util.Calendar
-import java.util.Locale
 
+@Suppress("MemberVisibilityCanBePrivate")
 data class ExpandedCoop(
     val id: Long = 0,
 
@@ -30,149 +30,91 @@ data class ExpandedCoop(
 
     val events: List<Event> = emptyList()
 ) {
-    val isStartEstimated = startTime == null
-    val isEndEstimated = endTime == null
+	val isStartEstimated = startTime == null
+	val isEndEstimated = endTime == null
 
-    val nowEpoch = Calendar.getInstance().timeInMillis / 1000L
+	val nowEpoch = Calendar.getInstance().timeInMillis / 1000L
 
-    val startEpoch = startTime?.let {
-        it.timeInMillis / 1000L
-    } ?: (nowEpoch % 3600)
+	val startEpoch = startTime?.let {
+		it.timeInMillis / 1000L
+	} ?: (nowEpoch % 3600)
 
-    val endEpoch = endTime?.let {
-        it.timeInMillis / 1000L
-    } ?: (startEpoch + 12 * 60 * 60)
+	val endEpoch = endTime?.let {
+		it.timeInMillis / 1000L
+	} ?: (startEpoch + 12 * 60 * 60)
 
-    val isEnded = nowEpoch > endEpoch
+	val isEnded = nowEpoch > endEpoch
 
-    val startInfoLine = if (isStartEstimated)
-        ":warning: No start time set, assuming start of current hour."
-    else
-        "<:contract_scroll_white:1291832469179338796> Start Time: <t:$startEpoch> (<t:$startEpoch:R>)"
+	val tvalNow = roundedTval(4, startEpoch, endEpoch, nowEpoch, 1)
+	val tval30Mins = roundedTval(4, startEpoch, endEpoch, nowEpoch + 30 * 60, 1)
+	val tval60Mins = roundedTval(4, startEpoch, endEpoch, nowEpoch + 60 * 60, 1)
+	
+	val tokensSent = mutableMapOf<String, Int>()
+	val tokensRec = mutableMapOf<String, Int>()
+	val tvalSent = mutableMapOf<String, Double>()
+	val tvalRec = mutableMapOf<String, Double>()
 
-    val endInfoLine = if (isEndEstimated)
-        ":warning: No end time set, assuming 12 hours from start time."
-    else
-        ":alarm_clock: End Time: <t:$endEpoch> (<t:$endEpoch:R>)"
+	val tvalDelta: Map<String, Double>
 
-    val tvalNow = roundedTval(4, startEpoch, endEpoch, nowEpoch, 1)
-    val tval30Mins = roundedTval(4, startEpoch, endEpoch, nowEpoch + 30 * 60, 1)
-    val tval60Mins = roundedTval(4, startEpoch, endEpoch, nowEpoch + 60 * 60, 1)
+	val selfTokensSent: Int
+	val selfTokensReceived: Int
+	val selfTvalSent: Double
+	val selfTvalReceived: Double
 
-    val futureTvalTable =
-        if (isEnded)
-            ":tada: Contract Complete!"
-        else listOfNotNull(
-            "__Running Token Value__",
-            "<:icon_token:653018008670961665> Now: `$tvalNow`",
-            if (tvalNow > 0.03) "In 30 minutes: `$tval30Mins`" else null,
-            if (tval30Mins > 0.03) "In 60 minutes: `$tval60Mins`" else null,
-        ).joinToString("\n")
+	val selfTvalDelta: Double
 
-    val generationInfoLine = "Report Generated at <t:$nowEpoch> (<t:$nowEpoch:R>)"
+	init {
+		var selfTokensSent = 0
+		var selfTokensReceived = 0
+		var selfTvalSent = 0.0
+		var selfTvalReceived = 0.0
 
-    val tokensSent = mutableMapOf<String, Int>()
-    val tokensRec = mutableMapOf<String, Int>()
-    val tvalSent = mutableMapOf<String, Double>()
-    val tvalRec = mutableMapOf<String, Double>()
+		for (event in events) with(event) {
+			val evTime: Long = time.timeInMillis / 1000L
+			val tv = tval(startEpoch, endEpoch, evTime, count)
 
-    val tvalDelta: Map<String, Double>
+			if (direction == Event.DIRECTION_SENT) {
+				tokensSent[person] = (tokensSent[person] ?: 0) + count
+				tvalSent[person] = (tvalSent[person] ?: 0.0) + tv
 
-    val selfTokensSent: Int
-    val selfTokensReceived: Int
-    val selfTvalSent: Double
-    val selfTvalReceived: Double
+				selfTokensReceived += count
+				selfTvalReceived += tv
+			} else {
+				tokensRec[person] = (tokensRec[person] ?: 0) + count
+				tvalRec[person] = (tvalRec[person] ?: 0.0) + tv
 
-    val selfTvalDelta: Double
+				selfTokensSent += count
+				selfTvalSent += tv
+			}
+		}
 
-    // Keyed by Player Name, value is table row.
-    val tvalTable: Map<String, String>
-    val tvalTableString: String
+		selfTvalDelta = selfTvalSent - selfTvalReceived
 
-    // TODO: Move the table code into the actual sink report.
-    // It's a mess where it is.
-    init {
-        var selfTokensSent = 0
-        var selfTokensReceived = 0
-        var selfTvalSent = 0.0
-        var selfTvalReceived = 0.0
+		tvalDelta = players.associateWith { player ->
+			(tvalSent[player] ?: 0.0) - (tvalRec[player] ?: 0.0)
+		}
 
-        for ((_, _, _, rawTime, count, person, direction) in events) {
-            val time: Long = rawTime.timeInMillis / 1000L
+		this.selfTokensSent = selfTokensSent
+		this.selfTokensReceived = selfTokensReceived
+		this.selfTvalSent = selfTvalSent
+		this.selfTvalReceived = selfTvalReceived
+	}
 
-            val tv = tval(startEpoch, endEpoch, time, count)
-            if (direction == Event.DIRECTION_SENT) {
-                tokensSent[person] = (tokensSent[person] ?: 0) + count
-                tvalSent[person] = (tvalSent[person] ?: 0.0) + tv
-
-                selfTokensReceived += count
-                selfTvalReceived += tv
-            } else {
-                tokensRec[person] = (tokensRec[person] ?: 0) + count
-                tvalRec[person] = (tvalRec[person] ?: 0.0) + tv
-
-                selfTokensSent += count
-                selfTvalSent += tv
-            }
-        }
-
-        selfTvalDelta = selfTvalSent - selfTvalReceived
-
-        tvalDelta = players.associateWith { player ->
-            (tvalSent[player] ?: 0.0) - (tvalRec[player] ?: 0.0)
-        }
-
-        this.selfTokensSent = selfTokensSent
-        this.selfTokensReceived = selfTokensReceived
-        this.selfTvalSent = selfTvalSent
-        this.selfTvalReceived = selfTvalReceived
-
-        // Keyed by Player Name, value is table row.
-        val tvalTable = mutableMapOf<String, String>()
-
-        for (person in players) {
-            val sent = tvalSent[person] ?: 0.0
-            val rec = tvalRec[person] ?: 0.0
-            val delta = sent - rec
-
-            val output = String.format(
-                Locale.US,
-                SINK_TABLE_FORMAT,
-                person,
-                delta,
-                tokensSent[person] ?: 0,
-                sent,
-                tokensRec[person] ?: 0,
-
-                // This looks odd, but it's to prevent -0.0 from showing up.
-                if (rec == 0.0) 0.0 else (rec * -1)
-            )
-            tvalTable[person] = output
-        }
-
-        this.tvalTable = tvalTable
-        tvalTableString = tvalTable.values.joinToString("\n")
-    }
-
-    val coop
-        get() = Coop(
-            id,
-            name,
-            contract,
-            startTime,
-            endTime,
-            sinkMode,
-            boostOrder,
-            players,
-            sink,
-            playerPositionOverrides,
-            playerOrderOverrides,
-            playerTokenAmounts
-        )
-
-    companion object {
-        const val SINK_TABLE_FORMAT = "%-12.12s|%9.3f|%4d|%8.3f|%4d|%9.3f"
-    }
+//	val coop
+//		get() = Coop(
+//			id,
+//			name,
+//			contract,
+//			startTime,
+//			endTime,
+//			sinkMode,
+//			boostOrder,
+//			players,
+//			sink,
+//			playerPositionOverrides,
+//			playerOrderOverrides,
+//			playerTokenAmounts
+//		)
 }
 
 fun Coop.expand(events: List<Event>) = ExpandedCoop(
--- app/src/main/java/net/itsjustsomedude/tokens/reports/SinkReport.kt
+++ app/src/main/java/net/itsjustsomedude/tokens/reports/SinkReport.kt
@@ -2,39 +2,81 @@ package net.itsjustsomedude.tokens.reports
 
 import net.itsjustsomedude.tokens.BuildConfig
 import net.itsjustsomedude.tokens.db.ExpandedCoop
-import net.itsjustsomedude.tokens.db.ExpandedCoop.Companion.SINK_TABLE_FORMAT
 
 class SinkReport : Report() {
-    override fun generate(data: ExpandedCoop): String {
-        return arrayOf(
-            "# __Tokification__",
-            "-# _${BuildConfig.VERSION_NAME}_",
-            "",
-            data.generationInfoLine,
-            "_This message can be manually updated by asking the sink politely._",
-            "",
-            "__Contract Info__",
-            data.startInfoLine,
-            data.endInfoLine,
-            "_Token values are only accurate once the end time is accurate._",
-            "",
-            data.futureTvalTable,
-            "",
-            "__Player's Current TVals__ (as seen by the :people_hugging: sink)",
-            "```",
-            "Player      |   Δ TVal| +TS|  +TSVal| -TR|   -TRVal",
-            "------------+---------+----+--------+----+---------",
-            data.tvalTable.values.joinToString("\n"),
-            SINK_TABLE_FORMAT.format(
-                "Sink",
-                data.selfTvalDelta,
-                data.selfTokensSent,
-                data.selfTvalSent,
-                data.selfTokensReceived,
-                data.selfTvalReceived
-            ),
-            "```",
-            "_This is not a Wonky command, but an app written by ItsJustSomeDude. Stay tuned for further updates!_",
-        ).joinToString("\n")
-    }
+	override fun generate(data: ExpandedCoop): String = with(data) {
+		val startInfoLine =
+			if (isStartEstimated) ":warning: No start time set, assuming start of current hour."
+			else "<:contract_scroll_white:1291832469179338796> Start Time: <t:$startEpoch> (<t:$startEpoch:R>)"
+
+		val endInfoLine =
+			if (isEndEstimated) ":warning: No end time set, assuming 12 hours from start time."
+			else ":alarm_clock: End Time: <t:$endEpoch> (<t:$endEpoch:R>)"
+
+		val futureTvalTable = if (isEnded) ":tada: Contract Complete!"
+		else listOfNotNull(
+			"__Running Token Value__",
+			"<:icon_token:653018008670961665> Now: `$tvalNow`",
+			if (tvalNow > 0.03) "In 30 minutes: `$tval30Mins`" else null,
+			if (tval30Mins > 0.03) "In 60 minutes: `$tval60Mins`" else null,
+		).joinToString("\n")
+
+		val generationInfoLine = "Report Generated at <t:$nowEpoch> (<t:$nowEpoch:R>)"
+
+		// Keyed by Player Name, value is table row.
+		val tvalTable = mutableMapOf<String, String>()
+
+		val sinkTableFormat = "%-12.12s|%9.3f|%4d|%8.3f|%4d|%9.3f"
+
+		for (person in players) {
+			val sent = tvalSent[person] ?: 0.0
+			val rec = tvalRec[person] ?: 0.0
+			val delta = sent - rec
+
+			val output = sinkTableFormat.format(
+				person,
+				delta,
+				tokensSent[person] ?: 0,
+				sent,
+				tokensRec[person] ?: 0,
+
+				// This looks odd, but it's to prevent -0.0 from showing up.
+				if (rec == 0.0) 0.0 else (rec * -1)
+			)
+			tvalTable[person] = output
+		}
+
+		val selfTableRow = sinkTableFormat.format(
+			"Sink",
+			data.selfTvalDelta,
+			data.selfTokensSent,
+			data.selfTvalSent,
+			data.selfTokensReceived,
+			data.selfTvalReceived
+		)
+
+		return arrayOf(
+			"# __Tokification__",
+			"-# _${BuildConfig.VERSION_NAME}_",
+			"",
+			generationInfoLine,
+			"_This message can be manually updated by asking the sink politely._",
+			"",
+			"__Contract Info__",
+			startInfoLine,
+			endInfoLine,
+			"_Token values are only accurate once the end time is accurate._",
+			"",
+			futureTvalTable,
+			"",
+			"__Player's Current TVals__ (as seen by the :people_hugging: sink)",
+			"```",
+			"Player      |   Δ TVal| +TS|  +TSVal| -TR|   -TRVal",
+			"------------+---------+----+--------+----+---------",
+			tvalTable.values.joinToString("\n"),
+			selfTableRow,
+			"```",
+			"_This is not a Wonky command, but an app written by ItsJustSomeDude. Stay tuned for further updates!_",
+		).joinToString("\n")
+	}
 }
