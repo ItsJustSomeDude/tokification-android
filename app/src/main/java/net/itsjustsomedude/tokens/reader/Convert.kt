867cd6d0899a2f4446c3650339bd8096b71025e4
Matthias Feldpausch <itsjustsomedude0@gmail.com>
2024-11-06 20:37:38 -0500

    Cleanup notification reader

--- app/src/main/java/net/itsjustsomedude/tokens/reader/Convert.kt
+++ app/src/main/java/net/itsjustsomedude/tokens/reader/Convert.kt
@@ -8,70 +8,69 @@ import java.util.regex.Pattern
 private const val TAG = "NoteConvert"
 
 private val personCoopRegex =
-    Pattern.compile("^(.+) \\((.+)\\) has (?:sent you|hatched).+?$")
+	Pattern.compile("^(.+) \\((.+)\\) has (?:sent you|hatched).+?$")
 private val tokenCountRegex = Pattern.compile("(?<=has sent you a gift of )([0-9]+)")
 private val ALLOWED_PACKAGES = listOf(
-    "com.auxbrain.egginc",
-    "net.itsjustsomedude.tokens",
-    "net.itsjustsomedude.tokens.debug"
+	"com.auxbrain.egginc",
+	"net.itsjustsomedude.tokens",
+	"net.itsjustsomedude.tokens.debug"
 )
 
 fun ShortNotification.toEvent(): Event? {
-    println("Relevant data: $packageName, $flags, $title, $text, $group, $id")
-    if (!ALLOWED_PACKAGES.contains(packageName)) return null
-    if ((flags and Notification.FLAG_GROUP_SUMMARY) != 0) return null
-    if (!title.contains("Gift Received")) return null
+	if (!ALLOWED_PACKAGES.contains(packageName)) return null
+	if ((flags and Notification.FLAG_GROUP_SUMMARY) != 0) return null
+	if (!title.contains("Gift Received")) return null
 
-    val matches = personCoopRegex.matcher(text)
-    if (!matches.lookingAt() || matches.groupCount() < 2) {
-        Log.e(TAG, "Person/Coop Regex didn't match for notification content: $text")
-        return null
-    }
+	val matches = personCoopRegex.matcher(text)
+	if (!matches.lookingAt() || matches.groupCount() < 2) {
+		Log.e(TAG, "Person/Coop Regex didn't match for notification content: $text")
+		return null
+	}
 
-    val person = matches.group(1)!!
-    val coopName = matches.group(2)!!
+	val person = matches.group(1)!!
+	val coopName = matches.group(2)!!
 
-    if (title.contains("🐣")) {
-        Log.i(TAG, "Processing CR")
-        return Event(
-            coop = coopName,
-            kevId = group,
-            person = person,
-            count = 0,      // Count of 0 indicates CR.
-            time = time,
-            direction = Event.DIRECTION_SENT,
-            notification = id
-        )
-    }
+	if (title.contains("🐣")) {
+		Log.i(TAG, "Processing CR")
+		return Event(
+			coop = coopName,
+			kevId = group,
+			person = person,
+			count = 0,      // Count of 0 indicates CR.
+			time = time,
+			direction = Event.DIRECTION_SENT,
+			notification = id
+		)
+	}
 
-    val amount: Int
-    if (text.contains("has sent you a Boost Token")) amount = 1
-    else {
-        try {
-            val countMatch = tokenCountRegex.matcher(text)
+	val amount: Int
+	if (text.contains("has sent you a Boost Token")) amount = 1
+	else {
+		try {
+			val countMatch = tokenCountRegex.matcher(text)
 
-            if (!countMatch.find() || countMatch.group(1) == null) {
-                Log.e(
-                    TAG,
-                    "Count Regex didn't match for notification content: $text"
-                )
-                return null
-            }
+			if (!countMatch.find() || countMatch.group(1) == null) {
+				Log.e(
+					TAG,
+					"Count Regex didn't match for notification content: $text"
+				)
+				return null
+			}
 
-            amount = countMatch.group(1)!!.toInt()
-        } catch (err: NumberFormatException) {
-            Log.e(TAG, "Bad number in note: $text")
-            return null
-        }
-    }
+			amount = countMatch.group(1)!!.toInt()
+		} catch (err: NumberFormatException) {
+			Log.e(TAG, "Bad number in note: $text")
+			return null
+		}
+	}
 
-    return Event(
-        coop = coopName,
-        kevId = group,
-        person = person,
-        count = amount,
-        time = time,
-        direction = Event.DIRECTION_SENT,
-        notification = id
-    )
+	return Event(
+		coop = coopName,
+		kevId = group,
+		person = person,
+		count = amount,
+		time = time,
+		direction = Event.DIRECTION_SENT,
+		notification = id
+	)
 }
